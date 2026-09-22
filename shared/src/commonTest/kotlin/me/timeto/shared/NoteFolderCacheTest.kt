a8427fe241c42c4dd8b4ba5dd4ca0548a1a0064c
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2026-09-22 22:22:13 +0800

    test: note folder cache lookup throws on missing folder

---
+++ shared/src/commonTest/kotlin/me/timeto/shared/NoteFolderCacheTest.kt
@@ -0,0 +1,31 @@
+package me.timeto.shared
+
+import me.timeto.shared.db.NoteDb
+import me.timeto.shared.db.NoteFolderDb
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class NoteFolderCacheTest {
+
+    @Test
+    fun selectFolderDbCached_missingFolder_throws() {
+        Cache.noteFoldersDb = emptyList()
+        val noteDb = NoteDb(id = 1, time = 1, sort = 0, folderId = 99, text = "x")
+        assertFailsWith<NoSuchElementException> {
+            noteDb.selectFolderDbCached()
+        }
+    }
+
+    @Test
+    fun selectFolderDbCached_presentFolder_returns() {
+        val folderDb = NoteFolderDb(
+            id = 5, time = 1, sort = 0, onHome = true,
+            symbol_raw = "icon--inbox", name = "Notes",
+        )
+        Cache.noteFoldersDb = listOf(folderDb)
+        val noteDb = NoteDb(id = 1, time = 1, sort = 0, folderId = 5, text = "x")
+        assertEquals(folderDb, noteDb.selectFolderDbCached())
+        Cache.noteFoldersDb = emptyList()
+    }
+}
